b6d8e59e5ebcafe718e36d9ba51f6a3cd23ae252
Sarsen <blazer.fei@gmail.com>
2024-10-03 19:36:34 +0500

    first commit

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,11 @@
+const AppInfo = () => {
+	return (
+		<div className="app-info">
+			<h1>Учет сотруднков в компаний Скала</h1>
+			<h2>Общее число сотруднков: </h2>
+			<h2>Премию получать: </h2>
+		</div>
+	)
+}
+
+export default AppInfo;
